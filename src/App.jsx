587ffa7f0831d0483e9eec14aa9d8842a5cc4eae
anishmj <mjanish19@gmail.com>
2024-07-20 17:02:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Currency } from './Currency'
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <Currency/>
+    </div>
+  )
+}
---
+++ src/Currency.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import axios from "axios";
+export const Currency = () => {
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(1);
+
+  async function fetchCurrency() {
+    try {
+      const response = await axios.get(
+        "https://api.frankfurter.app/currencies"
+      );
+    //   const data = await response.json();
+    setCurrencies(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    fetchCurrency();
+  }, []);
+  console.log(currencies)
+  //currencies - >https://api.frankfurter.app/currencies
+  //currencies - >https://api.frankfurter.app/latest?amount=1&from=USD$to=INR
+  //h-80 w-80 border-solid border-8
+  return (
+    <div className="flex justify-center pt-12">
+      <div className="h-96 w-9/12 bg-amber-200 rounded-lg shadow-lg">
+        <div>
+          <h1 className="font-bold text-2xl">Currency Converter</h1>
+        </div>
+        <div className="flex justify-start pl-4 pt-32 rounded-lg">
+          <div>
+            <label htmlFor="amount">Amount : </label>
+            <input type="number" />
+          </div>
+          <div>
+            <div>
+                <button>
+              <label htmlFor="convert">Convert : </label>
+              <input type="number" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="mb-4 text-lg font-medium text-right text-green">
+          Converted Amount : 69$
+        </div>
+      </div>
+    </div>
+  );
+};
