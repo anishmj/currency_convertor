b2549dc4fbba73e3831f22a05ed7f02ffbe9b1b9
anishmj <mjanish19@gmail.com>
2024-07-20 19:52:51 +0530

    conversion_done

--- src/Currency.jsx
+++ src/Currency.jsx
@@ -6,18 +6,18 @@ export const Currency = () => {
   const [currencies, setCurrencies] = useState([]);
   const [amount, setAmount] = useState(1);
 
-  const [fromCurrency,setfromCurrency] = useState("USD");
-  const [toCurrency,settoCurrency] = useState("INR");
-  const [converted,setConverted] = useState(null)
-  const [converting,setConverting] = useState(false)
-  
+  const [fromCurrency, setfromCurrency] = useState("USD");
+  const [toCurrency, settoCurrency] = useState("INR");
+  const [converted, setConverted] = useState(null);
+  const [converting, setConverting] = useState(false);
+
   async function fetchCurrency() {
     try {
       const response = await axios.get(
         "https://api.frankfurter.app/currencies"
       );
       // const data = await response.json();
-    setCurrencies(Object.keys(response.data));
+      setCurrencies(Object.keys(response.data));
     } catch (error) {
       console.error(error);
     }
@@ -25,27 +25,25 @@ export const Currency = () => {
   useEffect(() => {
     fetchCurrency();
   }, []);
-  console.log(currencies)
-async function convertCurrency(){
+  console.log(currencies);
+  async function convertCurrency() {
+    if (!amount) return;
+    setConverting(true);
 
-    if(!amount) return
-    setConverting(true)
-    
     try {
       const response = await axios.get(
         `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
       );
       // const data = await(response.json)
-      
-    setConverted(response.data.rates[toCurrency] + " " + toCurrency
-    )
+
+      setConverted(response.data.rates[toCurrency] + " " + toCurrency);
     } catch (error) {
       console.error(error);
-    }finally{
-      setConverting(false)
+    } finally {
+      setConverting(false);
     }
   }
-  
+
   //currencies - >https://api.frankfurter.app/currencies
   //currencies - >https://api.frankfurter.app/latest?amount=1&from=USD$to=INR
   //h-80 w-80 border-solid border-8
@@ -55,37 +53,47 @@ async function convertCurrency(){
         <div>
           <h1 className="font-bold text-2xl">Currency Converter</h1>
           <div>
-          <Dropdown currencies={currencies} title = "From" currency={fromCurrency} setCurrency={setfromCurrency}/>
-              {/* swap Currency */} 
-          <Dropdown currencies={currencies} title="To" currency={toCurrency} setCurrency={settoCurrency}/>
+            <Dropdown
+              currencies={currencies}
+              title="From"
+              currency={fromCurrency}
+              setCurrency={setfromCurrency}
+            />
+            {/* swap Currency */}
+            <Dropdown
+              currencies={currencies}
+              title="To"
+              currency={toCurrency}
+              setCurrency={settoCurrency}
+            />
+          </div>
         </div>
-        </div>
-       
+
         <div className="flex justify-start pl-4 pt-32 rounded-lg">
           <div>
             <label htmlFor="amount">Amount : </label>
             <input
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          type="number"
-          />
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              type="number"
+            />
           </div>
           <div>
             <div>
-                <button onClick={convertCurrency} className={converting? "animate-pulse":""}>
-              
-                Convert : 
-              
+              <button
+                onClick={convertCurrency}
+                className={converting ? "animate-pulse" : ""}
+              >
+                Convert :
               </button>
             </div>
           </div>
         </div>
         {converted && (
-           <div className="mb-4 text-lg font-medium text-right text-green">
-           Converted Amount : {converted}
-         </div>
+          <div className="mb-4 text-lg font-medium text-right text-green">
+            Converted Amount : {converted}
+          </div>
         )}
-       
       </div>
     </div>
   );
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default function Dropdown({
-    currencies,
-    currency,
-    setCurrency,
-    fav,
-    handlefav,
-    title=""
+  currencies,
+  currency,
+  setCurrency,
+  fav,
+  handlefav,
+  title = "",
 }) {
-    
-
   return (
     <div>
-        <label htmlFor={title}>{title}</label>
-        <div>
-            <select value={currency} onChange={(e)=>setCurrency(e.target.value)} className='w-36 p-2 rounded-md focus:outline-none focus:ring focus:ring-violet-300'>
-                
-                {currencies?.map((currency)=>{
-                   return( <option value={currency} key={currency}>
-                        {currency}
-                    </option>
-                   )
-                })}
-            </select>
-            
-        </div>
+      <label htmlFor={title}>{title}</label>
+      <div>
+        <select
+          value={currency}
+          onChange={(e) => setCurrency(e.target.value)}
+          className="w-36 p-2 rounded-md focus:outline-none focus:ring focus:ring-violet-300"
+        >
+          {currencies?.map((currency) => {
+            return (
+              <option value={currency} key={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </div>
-  )
+  );
 }
